c4befe040ae05addae5c340f2a739b9890ccb1d8
turkey-kim <rlaxmrgml@naver.com>
2023-08-03 16:36:57 +0900

    refactor: ObserverApi.js 반복코드 개선 및 파일명 변경  & 다수 애니메이션 클래스 통일

---
+++ animation/fadeOut.js
@@ -0,0 +1,26 @@
+// fadeout-target
+
+const targets = document.getElementsByClassName("fadeout-target");
+
+const section_loan_text01 = document.getElementById("section-loan__desc-01");
+const section_loan_text02 = document.getElementById("section-loan__desc-02");
+const section_loan_text03 = document.getElementById("section-loan__desc-03");
+
+const fadeOut = (e) => {
+  e.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(-3rem)";
+    }
+  });
+};
+
+const observer = new IntersectionObserver(fadeOut);
+
+for (let i = 0; i < targets.length; i++) {
+  observer.observe(targets[i]);
+}
+
+observer.observe(section_loan_text01);
+observer.observe(section_loan_text02);
+observer.observe(section_loan_text03);
