caf41a169e8b71100753ad91caf09c68b8ba52dc
turkey-kim <rlaxmrgml@naver.com>
2023-08-03 16:46:42 +0900

    comment: fadeOut.js 주석추가

--- animation/fadeOut.js
+++ animation/fadeOut.js
@@ -1,10 +1,12 @@
-// fadeout-target
+// fadeout-animation target
 
 const targets = document.getElementsByClassName("fadeout-target");
 
 const section_loan_text01 = document.getElementById("section-loan__desc-01");
 const section_loan_text02 = document.getElementById("section-loan__desc-02");
 const section_loan_text03 = document.getElementById("section-loan__desc-03");
+
+// fadeout-animation function
 
 const fadeOut = (e) => {
   e.forEach((entry) => {
@@ -15,8 +17,11 @@ const fadeOut = (e) => {
   });
 };
 
+// intersection-observer API code
+
 const observer = new IntersectionObserver(fadeOut);
 
+// observe animation targets
 for (let i = 0; i < targets.length; i++) {
   observer.observe(targets[i]);
 }
